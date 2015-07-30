c57a27ac00585a477b077ac5d04aa9868a6192ba
Phil Monroe <phil@philmonroe.com>
2015-07-29 20:05:50 -0700

    including template

---
+++ js/main.js
@@ -0,0 +1,8 @@
+---
+---
+
+{% include_relative vendor/jquery.min.js                   %}
+{% include_relative vendor/bootstrap.min.js                %}
+{% include_relative vendor/stickUp.min.js                  %}
+{% include_relative vendor/colorbox/jquery.colorbox-min.js %}
+{% include_relative vendor/templatemo_script.js            %}
