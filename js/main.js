e47998689ad0a556318d64361a1a148be62f066b
Rakefire <login@rakefire.io>
2015-11-29 16:56:42 +0000

    Updated 'js/main.js' via CloudCannon

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@
 
 {% include_relative vendor/jquery.min.js                   %}
 {% include_relative vendor/bootstrap.min.js                %}
-{% include_relative vendor/stickUp.min.js                  %}
+{% include_relative vendor/stickup.min.js                  %}
 {% include_relative vendor/colorbox/jquery.colorbox-min.js %}
 {% include_relative vendor/templatemo_script.js            %}
